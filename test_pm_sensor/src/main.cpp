1d4d55021dcf22cf5a89d26711f47c6c9896be9e
ninomerlino <lorenzomerlino9@gmail.com>
2022-03-14 19:22:34 +0100

    no more network class NET namespace is my bestfriend now| wifi not working for some reason

--- test_pm_sensor/src/main.cpp
+++ test_pm_sensor/src/main.cpp
@@ -1,23 +1,48 @@
-#include<Arduino.h>
-#include<network.hpp>
-#include<WiFi.h>
+#include <Arduino.h>
+#include <WiFi.h>
+#include<utility/wifi_drv.h>
+namespace NET {
+  void listConnections() {
+    uint8_t conn_count = WiFi.scanNetworks();
+    switch (conn_count) {
+    case WL_FAILURE:
+      Serial.println("Failed to scan for networks");
+      break;
+    case 0:
+      Serial.println("No networks found");
+      break;
+    default:
+      Serial.printf("Found %d networks:\n");
+      for (uint8_t i = 0; i < conn_count; i++)
+        Serial.printf("\t(%d) : %s\n", i, WiFiDrv::getSSIDNetoworks(i));
+      break;
+    }
+  }
+
+  void startConnection(char* ssid, const char* pass) {
+    if (WiFi.status() == WL_CONNECTED)
+      WiFi.disconnect();
+    if (WiFi.begin(ssid, pass) == WL_CONNECTED) {
+      Serial.println("Connection was successful");
+    } else {
+      Serial.println("Connection failed");
+    }
+  }
+}
 
 void setup() {
-  //serial setup
+  // serial setup
   Serial.begin(9600);
-  Serial.println("Serial setup completed");
-  Serial.printf("Wifi lib has found %d networks\n",WiFi.scanNetworks());
-  //wifi setup
-  Network::Setup("wifiname","wifipassword");
-  Network::PrintAvailableNetworks();
-  Network::Connect();
+  Serial.printf("|%s|\n", WiFi.firmwareVersion());
+  NET::listConnections();
+  NET::startConnection("Uwu", "carsound");
 }
 
 void loop() {
-  if(Network::GetStatus() == WL_CONNECTED){
-    Network::PrintNetworkInfo();
-  }else{
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("WoW very much connection so much internet");
+  } else {
     Serial.println("Error no connection detected");
-    delay(10000);
   }
+  delay(10000);
 }
