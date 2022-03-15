a1b29982bcc466e084a3dd29309a22ebc919984b
ninomerlino <lorenzomerlino9@gmail.com>
2022-03-15 16:51:34 +0100

    wifi working now next is socket client

--- test_pm_sensor/src/main.cpp
+++ test_pm_sensor/src/main.cpp
@@ -1,48 +1,53 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include<utility/wifi_drv.h>
 namespace NET {
-  void listConnections() {
-    uint8_t conn_count = WiFi.scanNetworks();
-    switch (conn_count) {
-    case WL_FAILURE:
-      Serial.println("Failed to scan for networks");
-      break;
-    case 0:
-      Serial.println("No networks found");
-      break;
-    default:
-      Serial.printf("Found %d networks:\n");
-      for (uint8_t i = 0; i < conn_count; i++)
-        Serial.printf("\t(%d) : %s\n", i, WiFiDrv::getSSIDNetoworks(i));
-      break;
-    }
-  }
-
-  void startConnection(char* ssid, const char* pass) {
-    if (WiFi.status() == WL_CONNECTED)
-      WiFi.disconnect();
-    if (WiFi.begin(ssid, pass) == WL_CONNECTED) {
-      Serial.println("Connection was successful");
-    } else {
-      Serial.println("Connection failed");
-    }
-  }
+	//debug stuff
+	void Scan() {
+		int16_t conn_count = WiFi.scanNetworks();
+		switch (conn_count) {
+		case WIFI_SCAN_FAILED:
+			Serial.println("Failed to scan for networks");
+			break;
+		case 0:
+			Serial.println("No networks found");
+			break;
+		case WIFI_SCAN_RUNNING:
+			Serial.println("Scan is running in async mode");
+			break;
+		default:
+			Serial.printf("Found %d networks:\n", conn_count);
+			for (uint8_t i = 0; i < conn_count; i++)
+				Serial.printf("\t(%d) : %s\n", i, WiFi.SSID(i).c_str());
+			break;
+		}
+	}
+	//usefull stuff
+	void Connect(char* ssid, const char* pass, int timeout = 60) {
+		WiFi.mode(WIFI_STA);
+		if (WiFi.status() == WL_CONNECTED)
+			WiFi.disconnect();
+		WiFi.begin(ssid, pass);
+		Serial.print("Connecting ");
+		for (int i = 0; i < timeout * 2; i++) {
+			delay(500);
+			Serial.print("#");
+			if (WiFi.status() == WL_CONNECTED)break;
+		}
+		if (WiFi.status() == WL_CONNECTED)
+			Serial.println("\nConnection was successful");
+		else
+			Serial.printf("\nConnection failed with status %d\n", WiFi.status());
+	}
 }
 
 void setup() {
-  // serial setup
-  Serial.begin(9600);
-  Serial.printf("|%s|\n", WiFi.firmwareVersion());
-  NET::listConnections();
-  NET::startConnection("Uwu", "carsound");
+	// serial setup
+	Serial.begin(9600);
+	Serial.println("");
+	NET::Scan();
+	NET::Connect("Uwu", "carsound");
 }
 
 void loop() {
-  if (WiFi.status() == WL_CONNECTED) {
-    Serial.println("WoW very much connection so much internet");
-  } else {
-    Serial.println("Error no connection detected");
-  }
-  delay(10000);
+	delay(10000);
 }
